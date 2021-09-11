569821bd9c3429e933d0aaf6c3aab349a5426ac3
Junaem <junh9547@gmail.com>
2021-09-11 17:42:25 +0900

    0916 inputs

---
+++ 0914/p_81301_junh.py
@@ -0,0 +1,31 @@
+roseta = {
+   	'zero': '0',
+   	'one': '1',
+   	'two': '2',
+   	'three': '3',
+   	'four': '4',
+   	'five': '5',
+   	'six': '6',
+   	'seven': '7',
+   	'eight': '8',
+   	'nine': '9'
+}
+
+def solution(s):
+    answer = ''
+    s = list(s)
+
+    while s:
+        c = s.pop(0)
+        if '0' <= c <= '9':
+            answer += (c)
+            continue
+        c_num = c
+        while c_num not in roseta:
+            c_num += s.pop(0)
+
+        answer += roseta[c_num]
+
+    return int(answer)
+
+print(solution("one4seveneight"))

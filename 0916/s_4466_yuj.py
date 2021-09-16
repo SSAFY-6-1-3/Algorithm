e4647e74d1d441868e3fa42881f77d3bd92f1943
김유정 <yujng789@naver.com>
2021-09-16 20:01:03 +0900

    0916

---
+++ 0916/s_4466_yuj.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('s_4466_input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int,input().split())
+    scores = sorted(list(map(int,input().split())))              # list 정렬
+    max_score = 0
+
+    for idx in range(1, K+1):                                   # 제일 큰 값 K개 더해서 쵀댓값 구하기
+        max_score += scores[-idx]
+
+    print('#{} {}'.format(tc, max_score))
+

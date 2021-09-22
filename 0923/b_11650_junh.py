5fbc2a055d72cdc0f271de47e57bd56dcbc3d24a
Junaem <52495192+Junaem@users.noreply.github.com>
2021-09-22 22:35:28 +0900

    0922 Junaem (#26)
    * junh
    
    * 0916 junh
    
    * 0917
    
    * 0922 junh

---
+++ 0923/b_11650_junh.py
@@ -0,0 +1,22 @@
+# def sortsort(li):
+#     # li.sort(key= lambda x: x[1])
+#     # li.sort(key= lambda x: x[0])
+#     li.sort(key= lambda x: (x[0], x[1]))
+#     return li
+#
+
+import sys
+input=sys.stdin.readline # 입력 속도 문제였다.
+
+N = int(input())
+li = []
+for _ in range(N):
+    li.append(list(map(int, input().split())))
+li.sort(key= lambda x: (x[0], x[1]))
+# sortsort(li)
+
+for i in range(N):
+    print(*li[i]) # 시간초과
+    # for k in range(2):
+    #     print(li[i][k], end=' ')
+    # print()

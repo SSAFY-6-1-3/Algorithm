7b074899bbe002be9a8369e32347d10f2bf250be
Junaem <junh9547@gmail.com>
2021-09-23 20:00:38 +0900

    0923 ljh

--- 0923/p_12901_junh.py
+++ 0923/p_12901_junh.py
@@ -1,10 +1,11 @@
 WEEKDAY = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
 MONTH =[0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 def solution(a, b):
-    days = b
+    days = b # 일 수에
     for m in range(a):
-        days += MONTH[m]
-    answer = WEEKDAY[days % 7]
+        days += MONTH[m] # 받은 월까지의 일 수를 더 한다.
+    answer = WEEKDAY[days % 7] # 7로 %연산
     return answer
 
 
--- 0923/p_42746_junh.py
+++ 0923/p_42746_junh.py
@@ -1,22 +1,22 @@
 def solution(numbers):
     answer = []
     length = len(numbers)
-    powers = [[numbers[i], 0] for i in range(length)]
+    powers = [[numbers[i], 0] for i in range(length)] # [숫자, 숫자 반복한 문자열]
 
     for i in range(length):
-        num = str(numbers[i])
+        num = str(numbers[i]) # numbers의 각 숫자
         n_len = len(num)
-        pow = ''
-        for k in range(4):
-            pow += num[k%n_len]
-        powers[i][1] = int(pow)
+        pow = '' # 앞에 붙어있을때 수의 가치
+        for k in range(4): # 1000(10의 네제곱) 
+            pow += num[k%n_len] # num을 반복하여 쌓기
+        powers[i][1] = int(pow) # num의 반복을 int로 변환하여 저장
 
-    powers.sort(key = lambda x : x[1], reverse=True)
+    powers.sort(key = lambda x : x[1], reverse=True) # 반복하여 int로 변환환 수의 크기로 내림차순 정렬
 
     for pow in powers:
-        answer += str(pow[0])
+        answer += str(pow[0]) # 순서대로 문자열의 형태로 쌓고
 
-    for _ in range(len(answer)-1):
+    for _ in range(len(answer)-1): # 앞에 의미없는 '0'제거
         if answer[0] == '0':
             answer.pop(0)
         else:

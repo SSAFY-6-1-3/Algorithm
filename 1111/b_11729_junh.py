35b95925be189cc8b0a148efe3c4c188dcc754f0
Junaem <52495192+Junaem@users.noreply.github.com>
2021-11-10 01:07:54 +0900

    delete .idea (#100)

--- 1111/b_11729_junh.py
+++ 1111/b_11729_junh.py
@@ -1,9 +1,14 @@
 n = int(input())
 
-hanoi = [0]*(n+1)
-hanoi[1] = 1
-
-for i in range(2, n+1):
-    hanoi[i] = hanoi[i-1]*2 +1
+# hanoi = [0]*(n+1)
+# hanoi[1] = 1
+#
+# for i in range(2, n+1):
+#     hanoi[i] = hanoi[i-1]*2 +1
 
 print(hanoi[n])
+
+def move(n, now, goal, mid):
+    if n==1:
+        print('{} {}'.format(now, goal))
+

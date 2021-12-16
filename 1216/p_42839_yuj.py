4a0b48f8a99c8ea7a126e602e93ad439d64233f0
김유정 <yujng789@naver.com>
2021-12-16 19:41:02 +0900

    42839수정

--- 1216/p_42839_yuj.py
+++ 1216/p_42839_yuj.py
@@ -44,17 +44,3 @@ def solution(numbers):
 
 numbers="17"
 print(solution(numbers))
-
-############################################
-# 다른 사람 풀이
-# 에러가 나지만(소수가 아예 없을 경우) 간단한 풀이
-
-# from itertools import permutations
-# def solution(n):
-#     a = set()
-#     for i in range(len(n)):
-#         a |= set(map(int, map("".join, permutations(list(n), i + 1))))
-#     a -= set(range(0, 2))
-#     for i in range(2, int(max(a) ** 0.5) + 1):
-#         a -= set(range(i * 2, max(a) + 1, i))
-#     return len(a)

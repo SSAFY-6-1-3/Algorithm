c4acbcbbc99d1766b28c9ed4f5aeda6626ccc3f4
somi <psomi0114@gmail.com>
2022-06-01 21:17:48 +0900

    0531 somi

---
+++ 220531/b_1759_somi.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+
+L, C = map(int, input().split())
+alphas = list(input().split())
+vowels = [i for i in alphas if i in ['a', 'e', 'i', 'o', 'u']]
+consts = list(set(alphas) - set(vowels))
+result = []
+
+for i in range(1, L - 1):  # 모음 뽑는 개수 i
+    vowel_comb = list(combinations(vowels, i))
+    const_comb = list(combinations(consts, L - i))  # 여기서 list 안바꾸고 for문 돌리면 안됨!!
+                                                    # 두번째 for문 돌때는 빈 리스트만 남음
+    if vowel_comb and const_comb:
+        for vowel in vowel_comb:  # (a,) (i,)
+            for const in const_comb:
+                password = list(vowel) + list(const)
+                password.sort()
+                result.append(''.join(password))
+
+result.sort()
+for ans in result:
+    print(ans)

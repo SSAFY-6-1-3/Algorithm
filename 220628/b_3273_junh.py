432509bdc104450db162516c5ec0dfa380079c49
junaem <junh9547@gmail.com>
2022-06-23 23:50:08 +0900

    220623 ljh

---
+++ 220628/b_3273_junh.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+li = list(map(int, input().split()))
+x = int(input())
+
+answer = 0
+nums = set()
+for i in range(n):
+    jjak = x - li[i]
+    if jjak in nums:
+        answer += 1
+    nums.add(li[i])
+
+
+print(answer)

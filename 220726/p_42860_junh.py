e1cf9541513edc99a538b9c5189cd5ff314ffefe
junaem <junh9547@gmail.com>
2022-07-23 23:24:51 +0900

    220723 junh

---
+++ 220726/p_42860_junh.py
@@ -0,0 +1,25 @@
+def solution(name):
+    answer = 0
+
+
+    def dfs(now, dir, move):
+        if now < -len(name) or now >= len(name): return 0
+        a = ord('A')
+        b = ord(name[now])
+
+        tmp = abs(b - a)
+        if 26 - tmp < tmp:
+            tmp = 26 - tmp
+
+        # move += dfs(now+dir, dir, move+1)
+        return move + tmp + dfs(now+dir, dir, 1)
+
+    print(dfs(0, 1, 0))
+    print(dfs(0, -1, 0))
+
+    return answer
+
+
+
+print(solution("JEROEN"))
+print(solution("JAN"))

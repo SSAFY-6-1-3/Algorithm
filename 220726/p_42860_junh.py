fabdd77c8f2a757383915a2fa6cdfe269aabc764
junaem <junh9547@gmail.com>
2022-07-25 23:41:30 +0900

    220725 junh

--- 220726/p_42860_junh.py
+++ 220726/p_42860_junh.py
@@ -1,23 +1,43 @@
 def solution(name):
-    answer = 0
+    up_down = 0
+    to_go = [0]
 
-
-    def dfs(now, dir, move):
-        if now <= -len(name) or now >= len(name): return 0
+    for i in range(len(name)):
+        chr = name[i]
         a = ord('A')
-        b = ord(name[now])
+        b = ord(chr)
 
         tmp = abs(b - a)
+        if tmp:
+            to_go.append(i)
+
         if 26 - tmp < tmp:
             tmp = 26 - tmp
+        up_down += tmp
 
-        # move += dfs(now+dir, dir, move+1)
-        return move + tmp + dfs(now+dir, dir, 1)
+    min_move = len(name) ** 2
 
-    print(dfs(0, 1, 0))
-    print(dfs(0, -1, 0))
+    def permu(visited, move):
+        nonlocal min_move
+        if move > min_move:
+            return
+        if len(visited) == len(to_go):
+            min_move = move
 
-    return answer
+        now_idx = visited[-1]
+
+        for next_idx in range(len(to_go)):
+            if next_idx in visited:
+                continue
+            visited.append(next_idx)
+            dist = abs(to_go[next_idx] - to_go[now_idx])
+            if dist > len(name) // 2 :
+                dist = len(name) - dist
+            permu(visited, move + dist)
+            visited.pop()
+
+    permu([0], 0)
+    return up_down + min_move
 
 
 

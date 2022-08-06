3d9c56f9121af371150cf9d2fa114ed21e5084f7
junaem <junh9547@gmail.com>
2022-08-06 23:32:24 +0900

    220806 junh

--- 220810/b_25215_junh.py
+++ 220810/b_25215_junh.py
@@ -1,9 +1,11 @@
-
 st = input()
-dp = [1] * len(st)
-print(dp)
+dp_up = [6001] * len(st)
+dp_down = [6001] * len(st)
+dp_up[0] = 2
+dp_down[0] = 1 + st[0].isupper()
 
 for i in range(1, len(st)):
-    dp[i] = dp[i-1] + 1
-    if st[i].isupper() != st[i-1].isupper():
-        if st[i-1].isupper() == st[i-2].isupper():
+    dp_up[i] = min(dp_up[i-1] + (st[i].islower()), dp_down[i-1] + 1) + 1
+    dp_down[i] = min(dp_down[i-1] + (st[i].isupper()), dp_up[i-1] + 1) + 1
+
+print(min(dp_up[-1], dp_down[-1]))

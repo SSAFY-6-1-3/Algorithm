5d5542fc2333f48e4d3d3f2939532dee271be246
junaem <junh9547@gmail.com>
2022-08-14 19:12:34 +0900

    220814 junh

--- 220816/b_11060_junh.py
+++ 220816/b_11060_junh.py
@@ -1,1 +1,14 @@
-.
+
+N = int(input())
+li = list(map(int, input().split()))
+dp = [1001] * N
+dp[0] = 0
+for i in range(N):
+    for j in range(1, li[i]+1):
+        if i + j < N:
+            dp[i+j] = min(dp[i+j], dp[i] + 1)
+
+if dp[-1] == 1001:
+    print(-1)
+else:
+    print(dp[-1])

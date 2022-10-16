b025340fa51fcfee0557def929798ff149d04f6e
junaem <junh9547@gmail.com>
2022-10-17 01:41:16 +0900

    221017 junh

--- 221011/b_15685_junh.py
+++ 221011/b_15685_junh.py
@@ -1,16 +1,28 @@
-
 N = int(input())
-starts = []
-for _ in range(N):
-    starts.append(list(map(int, input().split())))
-
+mat = [[False] * 101 for _ in range(101)]
 dY = (0, -1, 0, 1)
 dX = (1, 0, -1, 0)
 
+for _ in range(N):
+    x, y, d, g = map(int, input().split())
+    mat[y][x] = True
+    y += dY[d]
+    x += dX[d]
+    mat[y][x] = True
+    stack = [d]
 
-print(starts)
+    for _ in range(g):
+        for i in range(len(stack)-1, -1, -1):
+            d = (stack[i] + 1) % 4
+            y += dY[d]
+            x += dX[d]
+            mat[y][x] = True
+            stack.append(d)
 
+cnt = 0
+for y in range(100):
+    for x in range(100):
+        if mat[y][x] and mat[y+1][x] and mat[y][x+1] and mat[y+1][x+1]:
+            cnt += 1
 
-def build(y, x, d, g, stack):
-
-    while stack:
+print(cnt)

1cdf90dbb628c0a286b7065998670f1528e02aba
junaem <junh9547@gmail.com>
2022-11-13 23:55:32 +0900

    221113 junh2

--- 221114/b_1958_junh.py
+++ 221114/b_1958_junh.py
@@ -2,3 +2,14 @@ A = ' ' + input()
 B = ' ' + input()
 C = ' ' + input()
 
+lcs = [[[0 for _ in range(len(C))] for _ in range(len(B))] for _ in range(len(A))]
+
+for a in range(1, len(A)):
+    for b in range(1, len(B)):
+        for c in range(1, len(C)):
+            if A[a] == B[b] and B[b] == C[c]:
+                lcs[a][b][c] = lcs[a-1][b-1][c-1] + 1
+            else:
+                lcs[a][b][c] = max(lcs[a-1][b][c], lcs[a][b-1][c], lcs[a][b][c-1])
+
+print(max(lcs[-1][-1]))

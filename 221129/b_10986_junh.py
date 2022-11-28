6079e51703fe77ca5163fc37c369e17f0ae85eb5
junaem <junh9547@gmail.com>
2022-11-28 15:25:15 +0900

    221128 junh

--- 221129/b_10986_junh.py
+++ 221129/b_10986_junh.py
@@ -1,26 +1,16 @@
-import copy
-
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
+A = list(map(int, input().split())) + [0]
 
-dp = [[-1] * M for _ in range(N)]
-
-answer = 0
-num = 0
+dp = [0] * M
 
 for i in range(N):
-    num = (num + A[i]) % M
-    if num == 0:
-        answer += 1
-    dp[num] = answer
+    num = (A[i-1] + A[i]) % M
+    A[i] = num
+    dp[num] += 1
 
-# for i in range(1, N):
-#     tmp = [0] * N
-#     for j in range(i, N):
-#         tmp[j] = (dp[j] - A[i-1]) % M
-#         if tmp[j] == 0:
-#             answer += 1
-#     dp = tmp
-print(dp)
+answer = dp[0]
+for num in dp:
+    answer += num * (num - 1) // 2
+
 print(answer)
 
